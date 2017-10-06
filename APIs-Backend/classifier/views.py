23c35c67bd22b224eeb5a729be347a618e8d1ed8
dushyant7917 <dushyant7917@gmail.com>
2017-10-06 17:07:55 +0530

    classification api gives herb results

--- APIs-Backend/classifier/views.py
+++ APIs-Backend/classifier/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route
@@ -11,6 +9,25 @@ from django.core.exceptions import ObjectDoesNotExist
 import base64
 from . import label_image
 import os
+from django.utils.text import slugify
+from elasticsearch import Elasticsearch
+import json
+from pymongo import MongoClient
+
+client = MongoClient('localhost:27017')
+db = client.herb_data
+
+es = Elasticsearch()
+
+states_data = {}
+with open('./data/states.txt', 'r') as states:
+    for s in states:
+        state_info = s.strip("\n").split('|')
+        states_data[state_info[0]] = {}
+        states_data[state_info[0]]['latitude'] = state_info[1]
+        states_data[state_info[0]]['longitude'] = state_info[2]
+
+states.close()
 
 # Create your views here.
 class ImageView(viewsets.ModelViewSet):
@@ -28,9 +45,84 @@ class ImageView(viewsets.ModelViewSet):
         os.remove(filename)
 
         if result[1] > 0.6:
-            return Response({"identified": True, "result": result}, status=status.HTTP_200_OK)
+            herb_data = db.information.find_one({"botanical_name": result[0].capitalize()})
+            herb_info = {}
+            if herb_data != None:
+                herb_info = {
+                    'parts_used': herb_data['parts_used'],
+                    'properties': herb_data['properties'],
+                    'places': herb_data['places'],
+                    'botanical_name': herb_data['botanical_name']
+                }
+
+                map_info = []
+                for s in herb_info['places']:
+                    marker = {}
+                    marker['latitude'] = states_data[s]['latitude']
+                    marker['longitude'] = states_data[s]['longitude']
+                    map_info.append(marker)
+
+                query = ""
+                for prop in herb_info['properties']:
+                    query += " " + prop.replace(" disorders", "")
+
+                search_doc = {
+                    "size": 6,
+                    "query": {
+                        "dis_max": {
+                            "queries": [
+                                {
+                                    "match": {
+                                        "parts_used": {
+                                            "query": query,
+                                            "boost": 4.0
+                                        }
+                                    }
+                                },
+                                {
+                                    "match": {
+                                        "properties": {
+                                            "query": query,
+                                            "boost": 4.0
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    "sort": [
+                        { "_score": { "order": "desc" }}
+                    ]
+                }
+
+                similar_properties = es.search(index = "herbs_info", doc_type = "doc", body = json.dumps(search_doc), sort = "_score")
+
+                recommendations = []
+                for similar in similar_properties['hits']['hits']:
+                    item = {}
+                    if similar['_source']['botanical_name'] != herb_data['botanical_name']:
+                        item['botanical_name'] = similar['_source']['botanical_name'].replace(" ","-").lower()
+                        recommendations.append(item)
+
+            if herb_data is not None:
+                content = {
+                    "found": True,
+                    "herb_data": herb_info,
+                    "map_info": map_info,
+                    "recommendations": recommendations
+                }
+                return Response({"identified": True, "result": content}, status=status.HTTP_200_OK)
+            else:
+                content = {
+                    "found": False,
+                    "herb_data": [],
+                    "map_info": [],
+                    "recommendations": []
+                }
+                return Response({"identified": True, "result": content}, status=status.HTTP_200_OK)
+
         else:
-            return Response({"identified": False, "result": "Sorry can't identify herb!" }, status=status.HTTP_200_OK)
+            return Response({"identified": False, "result": {} }, status=status.HTTP_200_OK)
 
     @detail_route(methods=['POST'])
     def image_info(self, request):
