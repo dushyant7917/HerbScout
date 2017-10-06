ebe919712ca36310322f6753c70c5752c727dac2
Rud156 <rudrangshu_1997@yahoo.com>
2017-10-07 00:46:42 +0530

    Updated Server

--- fake_server/routes/search.js
+++ fake_server/routes/search.js
@@ -23,101 +23,95 @@ router.get('/plant_info', (req, res) => {
         }
     };
 
-    // request(options, (error, response, body) => {
-    //     if (error) {
-    //         console.log(error);
-    //         return res.json({
-    //             success: false,
-    //             message: 'Unable to get the data. Please try again later'
-    //         });
-    //     }
+    request(options, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return res.json({
+                success: false,
+                message: 'Unable to get the data. Please try again later'
+            });
+        }
 
-    //     if (response.statusCode >= 200 && response.statusCode < 400) {
-    //         console.log(body);
-    //         return res.json({
-    //             success: true,
-    //             data: body
-    //         });
-    //     } else {
-    //         return res.json({
-    //             success: false,
-    //             message: 'Something happened at our end. Sorry about that...'
-    //         });
+        if (response.statusCode >= 200 && response.statusCode < 400) {
+            // console.log(body);
+            let results = body.results.map(element => {
+                return {
+                    botanical_name: element.botanical_name,
+                    image: `http://localhost:3000/PlantImages/${element.botanical_name}/0.jpg`
+                };
+            });
+            return res.json({
+                success: true,
+                results: results
+            });
+        } else {
+            return res.json({
+                success: false,
+                message: 'Something happened at our end. Sorry about that...'
+            });
+        }
+    });
+
+    // var results = [{
+    //         botanical_name: 'aristolochia-bracteata'
+    //     },
+    //     {
+    //         botanical_name: 'artanema-longifolia'
+    //     },
+    //     {
+    //         botanical_name: 'balsamodendron-mukul'
+    //     },
+    //     {
+    //         botanical_name: 'berberis-asiatica'
+    //     },
+    //     {
+    //         botanical_name: 'aloe-indica'
+    //     },
+    //     {
+    //         botanical_name: 'abies-webbiana'
+    //     },
+    //     {
+    //         botanical_name: 'enicostemma-hyssopifolium'
+    //     },
+    //     {
+    //         botanical_name: 'garcinia-morella'
+    //     },
+    //     {
+    //         botanical_name: 'gloriosa-superba'
+    //     },
+    //     {
+    //         botanical_name: 'habenaria-edgeworthii'
+    //     },
+    //     {
+    //         botanical_name: 'kirganelia-reticulata'
+    //     },
+    //     {
+    //         botanical_name: 'piper-wallichii'
+    //     },
+    //     {
+    //         botanical_name: 'peganum-harmala'
+    //     },
+    //     {
+    //         botanical_name: 'rhabdia-lycioides'
+    //     },
+    //     {
+    //         botanical_name: 'rotula-aquatica'
+    //     },
+    //     {
+    //         botanical_name: 'xyris-indica'
+    //     },
+    //     {
+    //         botanical_name: 'barleria-longifolia'
+    //     },
+    //     {
+    //         botanical_name: 'acacia-concinna'
     //     }
-    // });
+    // ];
 
     // res.json({
     //     success: true,
-    //     data:
+    //     results: results
     // });
-
-    var results = [{
-            botanical_name: 'aristolochia-bracteata'
-        },
-        {
-            botanical_name: 'artanema-longifolia'
-        },
-        {
-            botanical_name: 'balsamodendron-mukul'
-        },
-        {
-            botanical_name: 'berberis-asiatica'
-        },
-        {
-            botanical_name: 'aloe-indica'
-        },
-        {
-            botanical_name: 'abies-webbiana'
-        },
-        {
-            botanical_name: 'enicostemma-hyssopifolium'
-        },
-        {
-            botanical_name: 'garcinia-morella'
-        },
-        {
-            botanical_name: 'gloriosa-superba'
-        },
-        {
-            botanical_name: 'habenaria-edgeworthii'
-        },
-        {
-            botanical_name: 'kirganelia-reticulata'
-        },
-        {
-            botanical_name: 'piper-wallichii'
-        },
-        {
-            botanical_name: 'peganum-harmala'
-        },
-        {
-            botanical_name: 'rhabdia-lycioides'
-        },
-        {
-            botanical_name: 'rotula-aquatica'
-        },
-        {
-            botanical_name: 'xyris-indica'
-        },
-        {
-            botanical_name: 'barleria-longifolia'
-        },
-        {
-            botanical_name: 'acacia-concinna'
-        }
-    ];
-
-    results = results.map(element => {
-        return {
-            botanical_name: element.botanical_name,
-            image: `http://localhost:3000/PlantImages/${element.botanical_name}/0.jpg`
-        };
-    });
-
-    res.json({
-        success: true,
-        results: results
-    });
 });
 
 router.get('/get_specific_plant', (req, res) => {
@@ -132,95 +126,91 @@ router.get('/get_specific_plant', (req, res) => {
 
     console.log(plantName);
 
-    // request(`${utilities.BASE_URL}/herb-info/${plantName}/`, (error, response, body) => {
-    //     if (error) {
-    //         console.log(error);
-    //         return res.json({
-    //             success: false,
-    //             message: 'Unable to get the data. Please try again later'
-    //         });
-    //     }
-
-    //     if (response.statusCode >= 200 && response.statusCode < 400) {
-    //         console.log(body);
-    //         if (!body.found) {
-    //             return res.json({
-    //                 success: false,
-    //                 message: 'Unable to find something with that name'
-    //             });
-    //         } else {
-    //             return res.json({
-    //                 success: true,
-    //                 data: body
-    //             });
-    //         }
-    //     } else {
-    //         return res.json({
-    //             success: false,
-    //             message: 'Something happened at our end. Sorry about that...'
-    //         });
-    //     }
-    // });
-
-    var data = {
-        'found': true,
-        'recommendations': [{
-                botanical_name: 'terminalia-paniculata'
-            },
-            {
-                botanical_name: 'terminalia-arjuna'
-            },
-            {
-                botanical_name: 'pueraria-tuberosa'
-            },
-            {
-                botanical_name: 'cleome-viscosa'
-            },
-            {
-                botanical_name: 'rheum-moorcroftianum'
-            }
-        ],
-        'map_info': [{
-                'latitude': '14.7504291',
-                'longitude': '78.57002559'
-            },
-            {
-                'latitude': '27.10039878',
-                'longitude': '93.61660071'
-            },
-            {
-                'latitude': '26.7499809',
-                'longitude': '94.21666744'
-            }
-        ],
-        'herb_data': {
-            'parts_used': [
-                'Bark'
-            ],
-            'properties': [
-                'Balance disorder',
-                'Bleeding disorders',
-                'Bipolar disorders',
-                'Bob disorders',
-                'Body dysmorphic disorder'
-            ],
-            'places': [
-                'Andhra Pradesh',
-                'Arunachal Pradesh',
-                'Assam'
-            ],
-            botanical_name: 'Azadirachta indica'
+    request(`${utilities.BASE_URL}/herb-info/${plantName}/`, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return res.json({
+                success: false,
+                message: 'Unable to get the data. Please try again later'
+            });
         }
-    };
 
-    data.herb_data.image = `http://localhost:3000/PlantImages/` +
-        `${utilities.hyphenateName(data.herb_data.botanical_name)}/0.jpg`;
-
-    return res.json({
-        success: true,
-        data: data
+        if (response.statusCode >= 200 && response.statusCode < 400) {
+            console.log(body);
+            if (!body.found) {
+                return res.json({
+                    success: false,
+                    message: 'Unable to find something with that name'
+                });
+            } else {
+                return res.json({
+                    success: true,
+                    data: body
+                });
+            }
+        } else {
+            return res.json({
+                success: false,
+                message: 'Something happened at our end. Sorry about that...'
+            });
+        }
     });
-    //TODO: Do something with request
+
+    // var data = {
+    //     'found': true,
+    //     'recommendations': [{
+    //             botanical_name: 'terminalia-paniculata'
+    //         },
+    //         {
+    //             botanical_name: 'terminalia-arjuna'
+    //         },
+    //         {
+    //             botanical_name: 'pueraria-tuberosa'
+    //         },
+    //         {
+    //             botanical_name: 'cleome-viscosa'
+    //         },
+    //         {
+    //             botanical_name: 'rheum-moorcroftianum'
+    //         }
+    //     ],
+    //     'map_info': [{
+    //             'latitude': '14.7504291',
+    //             'longitude': '78.57002559'
+    //         },
+    //         {
+    //             'latitude': '27.10039878',
+    //             'longitude': '93.61660071'
+    //         },
+    //         {
+    //             'latitude': '26.7499809',
+    //             'longitude': '94.21666744'
+    //         }
+    //     ],
+    //     'herb_data': {
+    //         'parts_used': [
+    //             'Bark'
+    //         ],
+    //         'properties': [
+    //             'Balance disorder',
+    //             'Bleeding disorders',
+    //             'Bipolar disorders',
+    //             'Bob disorders',
+    //             'Body dysmorphic disorder'
+    //         ],
+    //         'places': [
+    //             'Andhra Pradesh',
+    //             'Arunachal Pradesh',
+    //             'Assam'
+    //         ],
+    //         botanical_name: 'Azadirachta indica'
+    //     }
+    // };
+
+    // return res.json({
+    //     success: true,
+    //     data: data
+    // });
 });
 
 router.post('/plant_image', (req, res) => {
@@ -240,94 +230,94 @@ router.post('/plant_image', (req, res) => {
         }
     };
 
-    // request(options, (error, response, body) => {
-    //     if (error) {
-    //         console.log(error);
-    //         return res.json({
-    //             success: false,
-    //             message: 'Unable to get the data. Please try again later'
-    //         });
-    //     }
-
-    //     if (response.statusCode >= 200 && response.statusCode < 400) {
-    //         console.log(body);
-    //         if (!body.identified) {
-    //             return res.json({
-    //                 success: false,
-    //                 message: 'Unable to identify the plant'
-    //             });
-    //         } else {
-    //             return res.json({
-    //                 success: true,
-    //                 data: body
-    //             });
-    //         }
-    //     } else {
-    //         return res.json({
-    //             success: false,
-    //             message: 'Something happened at our end. Sorry about that'
-    //         });
-    //     }
-    // });
-
-    var data = {
-        'identified': true,
-        'result': {
-            'found': true,
-            'recommendations': [{
-                    botanical_name: 'terminalia-paniculata'
-                },
-                {
-                    botanical_name: 'terminalia-arjuna'
-                },
-                {
-                    botanical_name: 'pueraria-tuberosa'
-                },
-                {
-                    botanical_name: 'cleome-viscosa'
-                },
-                {
-                    botanical_name: 'rheum-moorcroftianum'
-                }
-            ],
-            'map_info': [{
-                    'latitude': '14.7504291',
-                    'longitude': '78.57002559'
-                },
-                {
-                    'latitude': '27.10039878',
-                    'longitude': '93.61660071'
-                },
-                {
-                    'latitude': '26.7499809',
-                    'longitude': '94.21666744'
-                }
-            ],
-            'herb_data': {
-                'parts_used': [
-                    'Bark'
-                ],
-                'properties': [
-                    'Balance disorder',
-                    'Bleeding disorders',
-                    'Bipolar disorders',
-                    'Bob disorders',
-                    'Body dysmorphic disorder'
-                ],
-                'places': [
-                    'Andhra Pradesh',
-                    'Arunachal Pradesh',
-                    'Assam'
-                ],
-                botanical_name: 'Azadirachta indica'
-            }
+    request(options, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return res.json({
+                success: false,
+                message: 'Unable to get the data. Please try again later'
+            });
         }
-    };
 
-    return res.json({
-        success: true,
-        data: data
+        if (response.statusCode >= 200 && response.statusCode < 400) {
+            console.log(body);
+            if (!body.identified) {
+                return res.json({
+                    success: false,
+                    message: 'Unable to identify the plant'
+                });
+            } else {
+                return res.json({
+                    success: true,
+                    data: body
+                });
+            }
+        } else {
+            return res.json({
+                success: false,
+                message: 'Something happened at our end. Sorry about that'
+            });
+        }
     });
+
+    // var data = {
+    //     'identified': true,
+    //     'result': {
+    //         'found': true,
+    //         'recommendations': [{
+    //                 botanical_name: 'terminalia-paniculata'
+    //             },
+    //             {
+    //                 botanical_name: 'terminalia-arjuna'
+    //             },
+    //             {
+    //                 botanical_name: 'pueraria-tuberosa'
+    //             },
+    //             {
+    //                 botanical_name: 'cleome-viscosa'
+    //             },
+    //             {
+    //                 botanical_name: 'rheum-moorcroftianum'
+    //             }
+    //         ],
+    //         'map_info': [{
+    //                 'latitude': '14.7504291',
+    //                 'longitude': '78.57002559'
+    //             },
+    //             {
+    //                 'latitude': '27.10039878',
+    //                 'longitude': '93.61660071'
+    //             },
+    //             {
+    //                 'latitude': '26.7499809',
+    //                 'longitude': '94.21666744'
+    //             }
+    //         ],
+    //         'herb_data': {
+    //             'parts_used': [
+    //                 'Bark'
+    //             ],
+    //             'properties': [
+    //                 'Balance disorder',
+    //                 'Bleeding disorders',
+    //                 'Bipolar disorders',
+    //                 'Bob disorders',
+    //                 'Body dysmorphic disorder'
+    //             ],
+    //             'places': [
+    //                 'Andhra Pradesh',
+    //                 'Arunachal Pradesh',
+    //                 'Assam'
+    //             ],
+    //             botanical_name: 'Azadirachta indica'
+    //         }
+    //     }
+    // };
+
+    // return res.json({
+    //     success: true,
+    //     data: data
+    // });
 });
 
 
